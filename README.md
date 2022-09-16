00fccb9c17a9c4e0250f71a035946b13d96c91c6
w2013 <w2013@e-mirim.hs.kr>
2022-09-16 11:07:50 +0900

    views, templates, urls notice:list [PART5] Gimmunity

--- README.md
+++ README.md
@@ -27,3 +27,9 @@
    5. **admin** 작업 (pw: 미림과학고)
       1. NoticeAdmin
       2. python manage.py createsuperuser
+   6. **views**
+      1. NoticeListView
+   7. **templates**
+      1. notice_list.html
+   8. **urls**
+      1. notice:list
--- notice/views.py
+++ notice/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from notice.models import Notice
+
+
+class NoticeListView(ListView):
+    model = Notice  #DB에서 notice 전체 가져와서 notice_list라는 변수로 notice_list.html파일로 전달
