e2559cd355798a8199cf451b32f3dab7e8b4faa8
w2013 <w2013@e-mirim.hs.kr>
2022-09-16 11:18:27 +0900

    views, templates, urls notice:detail [PART6] Gimmunity

--- notice/views.py
+++ notice/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from notice.models import Notice
 
 
 class NoticeListView(ListView):
     model = Notice  #DB에서 notice 전체 가져와서 notice_list라는 변수로 notice_list.html파일로 전달
+
+
+class NoticeDetailView(DetailView):
+    model=Notice #DB에서 pk에 해당하는 notice 가져와서 --> notice라는 변수로 --> notice detail.html 파일로 전달
